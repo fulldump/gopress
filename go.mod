e7e4be87fab23a37c8b5ffc7c891c117652a2d92
fulldump <gerardooscarjt@gmail.com>
2023-04-26 01:14:04 +0200

    feat: draft API + go mod
    Co-authored-by: Alfonso Cantos <alfonso.cantos@gmail.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gopress
+
+go 1.20
+
+require github.com/fulldump/box v0.3.0
--- main.go
+++ main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/fulldump/box"
 )
 
 func main() {
 
+	b := box.NewBox()
+
+	b.Handle("GET", "/v1/articles", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: list articles"
+	})
+
+	b.Handle("POST", "/v1/articles", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: create article"
+	})
+
+	b.Handle("GET", "/v1/articles/{articleId}", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: get article"
+	})
+
+	b.Handle("PATCH", "/v1/articles/{articleId}", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: modify article"
+	})
+
+	b.Handle("DELETE", "/v1/articles/{articleId}", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: delete article"
+	})
+
+	b.Handle("POST", "/v1/articles/{articleId}/publish", func(w http.ResponseWriter, r *http.Request) string {
+		return "todo: publish article"
+	})
+
 	server := http.Server{
-		Addr: ":9955",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello, this is gopress.org!!"))
-		}),
+		Addr:    ":9955",
+		Handler: b,
 	}
 
+	fmt.Println("Listening on", server.Addr)
 	server.ListenAndServe()
 }
