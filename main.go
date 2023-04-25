d748e0e85111a01cc7543f4ee8df5685079af7e3
fulldump <gerardooscarjt@gmail.com>
2023-04-26 01:25:57 +0200

    feat: implement list and create articles
    Co-authored-by: Alfonso Cantos <alfonso.cantos@gmail.com>

--- main.go
+++ main.go
@@ -3,20 +3,67 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/fulldump/box"
 )
+
+type Article struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+
+	CreatedOn time.Time `json:"createdOn"`
+	Published bool      `json:"published"` // todo: use date to program publishment in the future
+}
+
+type ArticleShort struct {
+	Id    string `json:"id"`
+	Title string `json:"title"`
+}
 
 func main() {
 
 	b := box.NewBox()
 
-	b.Handle("GET", "/v1/articles", func(w http.ResponseWriter, r *http.Request) string {
-		return "todo: list articles"
+	articles := map[string]*Article{}
+
+	b.Handle("GET", "/v1/articles", func() any {
+
+		result := []*ArticleShort{}
+
+		for id, article := range articles {
+			result = append(result, &ArticleShort{
+				Id:    id,
+				Title: article.Title,
+			})
+		}
+
+		return result
 	})
 
-	b.Handle("POST", "/v1/articles", func(w http.ResponseWriter, r *http.Request) string {
-		return "todo: create article"
+	type CreateArticleRequest struct {
+		Id    string `json:"id"`
+		Title string `json:"title"`
+	}
+
+	b.Handle("POST", "/v1/articles", func(input *CreateArticleRequest) any {
+
+		if _, exist := articles[input.Id]; exist {
+			return map[string]interface{}{
+				"error": "article id '" + input.Id + "' already exists",
+			}
+		}
+
+		newArticle := &Article{
+			Title:     input.Title,
+			Content:   "Start here",
+			CreatedOn: time.Now(),
+			Published: false,
+		}
+
+		articles[input.Id] = newArticle
+
+		return newArticle
 	})
 
 	b.Handle("GET", "/v1/articles/{articleId}", func(w http.ResponseWriter, r *http.Request) string {
